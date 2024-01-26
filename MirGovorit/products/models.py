5ea56d193525298a013d5814b7ea579d0c317509
Roman Nevzorov <pomkanevzorov@gmail.com>
2024-01-27 02:13:49 +0500

    Created models and methods.

--- MirGovorit/products/models.py
+++ MirGovorit/products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=64,
+        null=False,
+        blank=False,
+    )
+    use_count = models.IntegerField(
+        default=0,
+        null=False,
+        blank=False,
+    )
+
+    def __str__(self) -> str:
+        return f"{self.name} - {self.use_count} usages"
+
+    def add_count(self) -> None:
+        self.use_count += 1
+        self.save()
--- MirGovorit/recipes/models.py
+++ MirGovorit/recipes/models.py
@@ -1,3 +1,42 @@
-from django.db import models
+from django.db import models, IntegrityError
 
-# Create your models here.
+from products.models import Product
+
+
+class Recipe(models.Model):
+    name = models.CharField(
+        max_length=64,
+        null=False,
+        blank=False,
+    )
+    products = models.ManyToManyField(
+        to=Product,
+        through='RecipeProducts',
+    )
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+
+class RecipeProducts(models.Model):
+    recipe = models.ForeignKey(
+        to=Recipe,
+        on_delete=models.CASCADE,
+    )
+    product = models.ForeignKey(
+        to=Product,
+        on_delete=models.CASCADE,
+    )
+    prod_amount = models.IntegerField(
+        default=0,
+        null=False,
+        blank=False,
+    )
+
+    def save(self, *args, **kwargs) -> None:
+        result = RecipeProducts.objects.filter(recipe=self.recipe, product=self.product)
+        if not result.exists():
+            super(RecipeProducts, self).save(*args, **kwargs)
+        else:
+            amount = result[0].product_amount
+            raise IntegrityError(f"This recipe already contains this product in amount of {amount}")
