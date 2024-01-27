80bfe0182b7f2b868cbbc3edefded434ef547ec2
Roman Nevzorov <pomkanevzorov@gmail.com>
2024-01-27 21:00:03 +0500

    Some models and methods fixes.

--- MirGovorit/products/models.py
+++ MirGovorit/products/models.py
@@ -4,14 +4,9 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(
         max_length=64,
-        null=False,
-        blank=False,
+        unique=True,
     )
-    use_count = models.IntegerField(
-        default=0,
-        null=False,
-        blank=False,
-    )
+    use_count = models.IntegerField(default=0)
 
     def __str__(self) -> str:
         return f"{self.name} - {self.use_count} usages"
--- MirGovorit/recipes/models.py
+++ MirGovorit/recipes/models.py
@@ -1,14 +1,10 @@
-from django.db import models, IntegrityError
+from django.db import models
 
 from products.models import Product
 
 
 class Recipe(models.Model):
-    name = models.CharField(
-        max_length=64,
-        null=False,
-        blank=False,
-    )
+    name = models.CharField(max_length=64)
     products = models.ManyToManyField(
         to=Product,
         through='RecipeProducts',
@@ -27,16 +23,14 @@ class RecipeProducts(models.Model):
         to=Product,
         on_delete=models.CASCADE,
     )
-    prod_amount = models.IntegerField(
-        default=0,
-        null=False,
-        blank=False,
-    )
+    prod_amount = models.IntegerField(default=0)
 
     def save(self, *args, **kwargs) -> None:
         result = RecipeProducts.objects.filter(recipe=self.recipe, product=self.product)
         if not result.exists():
             super(RecipeProducts, self).save(*args, **kwargs)
         else:
-            amount = result[0].prod_amount
-            raise IntegrityError(f"This recipe already contains this product in amount of {amount}")
+            obj = result[0]
+            amount = obj.prod_amount
+            if self.prod_amount != amount:
+                result.update(prod_amount=self.prod_amount)
